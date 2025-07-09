cee128196af0a35c1fc2f4415375d530b977f0e0
Shreya16032005 <shreya16032005@gmail.com>
2025-07-09 16:22:24 +0530

    Create First.js

---
+++ First.js
@@ -0,0 +1,24 @@
+let a=60
+let b=20
+let sum=a+b
+console.log(sum)
+80
+undefined
+let a=60
+let b=20
+let sub=a-b
+console.log(sub)
+40
+undefined
+let a=60
+let b=20
+let mul=a*b
+console.log(mul)
+1200
+undefined
+let a=60
+let b=20
+let div=a/b
+console.log(div)
+3
+undefined
